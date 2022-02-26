3f2de0d44d16129d7b2bc2e282a506fbe610dd3f
Aditya Thakur <18bce242@nirmauni.ac.in>
2022-02-26 19:48:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import CompanyData from "./components/CompanyData/CompanyData"
+
+function App() {
+  return (
+    <div className="main-constainer">
+      <div className="sub-container">
+        <CompanyData/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CompanyData/CompanyData.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CompanyData() {
+    
+}
+
+export default CompanyData;
